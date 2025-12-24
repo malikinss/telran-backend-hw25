d3f39fd29a74a92b58494cc44d4184fd967b9220
Sam Malikin <workmalikin@gmail.com>
2025-12-24 20:40:57 +0200

    update tests/**init**.py to include NumberBox tests
    * Added `TestNumberBox` to `__all__` for proper test discovery
    * Ensures both `MyStackInt` and `NumberBox` test classes are exported

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,6 @@
-# tests/__init__.py
+# ./tests/__init__.py
 
+from .test_my_stack_int import TestMyStackInt
+from .test_number_box import TestNumberBox
 
-__all__ = []
+__all__ = ["TestMyStackInt", "TestNumberBox"]
